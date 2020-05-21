f1f975d56051fef5697d881b85f24df574671bfa
Oliver Veits <oveits@users.noreply.github.com>
2020-05-21 11:59:49 +0200

    allow VNC access from outside

--- startup.sh
+++ startup.sh
@@ -3,5 +3,7 @@ printf "$PASSWORD\n$PASSWORD\n\n" | vncpasswd;
 vncserver :1;
 vncserver -kill :1;
 echo "code --user-data-dir /; wmctrl -i -r 0x800001 -e 0,0,0,$WIDTH,$HEIGHT" >> ~/.vnc/xstartup;
-vncserver -localhost -depth 24 -geometry $WIDTH'x'$HEIGHT :1;
+#vncserver -localhost -depth 24 -geometry $WIDTH'x'$HEIGHT :1;
+# allow VNC access from outside:
+vncserver -depth 24 -geometry $WIDTH'x'$HEIGHT :1;
 /noVNC/utils/launch.sh --vnc localhost:5901;
