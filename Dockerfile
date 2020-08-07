1a011efa40e8d56dcf1c972830d44cf423231506
Oliver Veits @ vocon <oliver.veits@vocon-it.com>
2020-08-07 17:54:56 +0000

    user 1000, persistence support

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,23 @@
 FROM ubuntu:16.04
 
 USER root
+ENV USER_HOME=/home/user
+ENV PASSWORD=123456 WIDTH=1920 HEIGHT=1080
+
+# install standard packages:
 RUN apt-get update; \
     apt-get -y upgrade; \
     apt-get install -y vnc4server git python vim wmctrl curl apt-transport-https libasound2 build-essential wget sudo
-RUN curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg; \
-    install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/; \
-    sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'; \
-    apt-get update; \
-    apt-get install -y code; \
-    sed -i 's/BIG-REQUESTS/_IG-REQUESTS/' /usr/lib/x86_64-linux-gnu/libxcb.so.1
-ENV PASSWORD=123456 WIDTH=1920 HEIGHT=1080
 
-# Install NodeJs/NPM:
+# install Visual Studio Code:
+RUN curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg \
+    && install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/ \
+    && sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list' \
+    && apt-get update \
+    && apt-get install -y code \
+    && sed -i 's/BIG-REQUESTS/_IG-REQUESTS/' /usr/lib/x86_64-linux-gnu/libxcb.so.1
 
+# Install NodeJs/NPM from source:
 # works, but installing from source takes very long:
 #RUN wget https://nodejs.org/dist/v10.16.0/node-v10.16.0.tar.gz; \
 #    tar -xzvf node-v10.16.0.tar.gz; \
@@ -23,17 +27,18 @@ ENV PASSWORD=123456 WIDTH=1920 HEIGHT=1080
 #    make install; \
 #    rm ../node-v10.16.0.tar.gz;
 
-# Install grout and user 1000, if it does not exist:
+# Install group and user 1000, if it does not exist:
 RUN getent group | grep ':1000:' || groupadd usergroup -g 1000
-RUN useradd -m user -u 1000 -g 1000 -d /home/user || true
+RUN useradd -m user -u 1000 -g 1000 -d $USER_HOME || true
 # RUN mkdir -p /root/.m2 && chown $MYUSER:$MYGROUP -R /root
 
+# Install NodeJs/NPM via NVM (must go after useradd)
 RUN curl -s -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | grep -v "sleep 20" | bash \
     && . "$HOME/.nvm/nvm.sh" \
     && LATEST_LTS=$(nvm ls-remote | grep "Latest LTS" | sed 's/^[^v]*\(v[0-9\.]*\)[^0-9].*$/\1/' | tail -1) \
     && nvm install $LATEST_LTS \
-    && cp -R $HOME/.nvm /home/user/ \
-    && chown user:usergroup -R /home/user/ \
+    && cp -R $HOME/.nvm $USER_HOME/ \
+    && chown 1000:1000 -R $USER_HOME/ \
     && cat ~/.bashrc | grep NVM_DIR >> /etc/bash.bashrc
 
 RUN . "$HOME/.nvm/nvm.sh" \
@@ -41,12 +46,14 @@ RUN . "$HOME/.nvm/nvm.sh" \
     && npm install -g @angular/cli@9.1.7
 
 COPY ./noVNC/ /noVNC/
-COPY ./startup.sh /startup.sh
-RUN chmod 777 /startup.sh; \
-    chmod 777 -R /noVNC
-USER user
-WORKDIR /home/user
+RUN chmod 777 -R /noVNC
 
+ADD entrypoint_vnc_startup.sh entrypoint_post_vnc_startup.sh /
+RUN chmod a+x /entrypoint*.sh
+
+## switch back to default user
+USER 1000
+WORKDIR $USER_HOME
 
 RUN . "$HOME/.nvm/nvm.sh" \
     && LATEST_LTS=$(nvm ls-remote | grep "Latest LTS" | sed 's/^[^v]*\(v[0-9\.]*\)[^0-9].*$/\1/' | tail -1) \
@@ -58,4 +65,5 @@ RUN . "$HOME/.nvm/nvm.sh" \
 #    && export NG_CLI_ANALYTICS=ci \
 #    && npm install -g @angular/cli@9.1.7
 
-ENTRYPOINT ["/startup.sh"]
+
+ENTRYPOINT ["/entrypoint_vnc_startup.sh", "/entrypoint_post_vnc_startup.sh"]
--- entrypoint_vnc_startup.sh
+++ entrypoint_vnc_startup.sh
@@ -1,142 +1,43 @@
-#!/bin/bash
-### every exit != 0 fails the script
-set -e
+#!/bin/sh
 
-## print out help
-help (){
-echo "
-USAGE:
-docker run -it -p 6901:6901 -p 5901:5901 consol/<image>:<tag> <option>
+VNC_ARGS=${VNC_ARGS:="-depth 24 -geometry ${WIDTH}x${HEIGHT} :1"}
 
-IMAGES:
-consol/ubuntu-xfce-vnc
-consol/centos-xfce-vnc
-consol/ubuntu-icewm-vnc
-consol/centos-icewm-vnc
+# set REMOTE_VNC to true, if direct VNC access from the Internet is allowed:
+REMOTE_VNC=${REMOTE_VNC:=true}
+[ "$REMOTE_VNC" != "true" ] && echo "$VNC_ARGS" | grep -q -v localhost && VNC_ARGS="-localhost $VNC_ARGS"
 
-TAGS:
-latest  stable version of branch 'master'
-dev     current development version of branch 'dev'
+# set VNC password:
+printf "$PASSWORD\n$PASSWORD\n\n" | vncpasswd
 
-OPTIONS:
--w, --wait      (default) keeps the UI and the vncserver up until SIGINT or SIGTERM will received
--s, --skip      skip the vnc startup and just execute the assigned command.
-                example: docker run consol/centos-xfce-vnc --skip bash
--d, --debug     enables more detailed startup output
-                e.g. 'docker run consol/centos-xfce-vnc --debug bash'
--h, --help      print out this help
+## Configure VNC Server via configuration file ($HOME/.vnc/xstartup)
+# create default xstartup file by starting and killing a vncserver:
+  rm $HOME/.vnc/xstartup || true
+  vncserver :1
+  vncserver -kill :1
+  # replace the (or add a) line to start visual studio code with the latest parameters:
+  cat $HOME/.vnc/xstartup | egrep -v '^code' > $HOME/.vnc/xstartup.new
 
-Fore more information see: https://github.com/ConSol/docker-headless-vnc-container
-"
-}
-if [[ $1 =~ -h|--help ]]; then
-    help
-    exit 0
+  cat <<'EOF' >> $HOME/.vnc/xstartup.new
+if test "$(id -u)" = "0"; then
+  # as root:
+  code --user-data-dir /root
+else
+  # as a normal user:
+  code
 fi
 
-# Adding information such as user, directory and git branch to the bash
-cat << 'EOF' >> $HOME/.bashrc
-
-parse_git_branch() {
-     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
-}
-export PS1='\[\e[1;32m\]\u@\h \[\e[1;33m\]\w \[\e[1;34m\]$(parse_git_branch)\[\e[00m\]\n$ '
-
+# find window ID:
+VSCODE_WINDOW_ID=$(xwininfo -tree -root | grep "Visual Studio Code" | head -1 | awk '{print $1}') 
+echo VSCODE_WINDOW_ID=$VSCODE_WINDOW_ID
 EOF
 
-# should also source $STARTUPDIR/generate_container_user
-source $HOME/.bashrc
+  echo '# resize Visual Studio Code Window:' >> $HOME/.vnc/xstartup.new
+  echo "wmctrl -i -r \${VSCODE_WINDOW_ID} -e 0,0,0,${WIDTH},${HEIGHT}" >> $HOME/.vnc/xstartup.new
+  cp $HOME/.vnc/xstartup.new $HOME/.vnc/xstartup
 
-# add `--skip` to startup args, to skip the VNC startup procedure
-if [[ $1 =~ -s|--skip ]]; then
-    echo -e "\n\n------------------ SKIP VNC STARTUP -----------------"
-    echo -e "\n\n------------------ EXECUTE COMMAND ------------------"
-    echo "Executing command: '${@:2}'"
-    exec "${@:2}"
-fi
-if [[ $1 =~ -d|--debug ]]; then
-    echo -e "\n\n------------------ DEBUG VNC STARTUP -----------------"
-    export DEBUG=true
-fi
+# Start VNC Server:
+vncserver $VNC_ARGS
 
-## correct forwarding of shutdown signal
-cleanup () {
-    kill -s SIGTERM $!
-    exit 0
-}
-trap cleanup SIGINT SIGTERM
+# Start noVNC Server:
+/noVNC/utils/launch.sh --vnc localhost:5901
 
-## write correct window size to chrome properties
-$STARTUPDIR/chrome-init.sh
-
-## resolve_vnc_connection
-VNC_IP=$(hostname -i)
-
-## change vnc password
-echo -e "\n------------------ change VNC password  ------------------"
-# first entry is control, second is view (if only one is valid for both)
-mkdir -p "$HOME/.vnc"
-PASSWD_PATH="$HOME/.vnc/passwd"
-
-if [[ -f $PASSWD_PATH ]]; then
-    echo -e "\n---------  purging existing VNC password settings  ---------"
-    rm -f $PASSWD_PATH
-fi
-
-if [[ $VNC_VIEW_ONLY == "true" ]]; then
-    echo "start VNC server in VIEW ONLY mode!"
-    #create random pw to prevent access
-    echo $(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 20) | vncpasswd -f > $PASSWD_PATH
-fi
-echo "$VNC_PW" | vncpasswd -f >> $PASSWD_PATH
-chmod 600 $PASSWD_PATH
-
-
-## start vncserver and noVNC webclient
-echo -e "\n------------------ start noVNC  ----------------------------"
-if [[ $DEBUG == true ]]; then echo "$NO_VNC_HOME/utils/launch.sh --vnc localhost:$VNC_PORT --listen $NO_VNC_PORT"; fi
-$NO_VNC_HOME/utils/launch.sh --vnc localhost:$VNC_PORT --listen $NO_VNC_PORT &> $STARTUPDIR/no_vnc_startup.log &
-PID_SUB=$!
-
-echo -e "\n------------------ start VNC server ------------------------"
-echo "remove old vnc locks to be a reattachable container"
-vncserver -kill $DISPLAY &> $STARTUPDIR/vnc_startup.log \
-    || rm -rfv /tmp/.X*-lock /tmp/.X11-unix &> $STARTUPDIR/vnc_startup.log \
-    || echo "no locks present"
-
-echo -e "start vncserver with param: VNC_COL_DEPTH=$VNC_COL_DEPTH, VNC_RESOLUTION=$VNC_RESOLUTION\n..."
-if [[ $DEBUG == true ]]; then echo "vncserver $DISPLAY -depth $VNC_COL_DEPTH -geometry $VNC_RESOLUTION"; fi
-vncserver $DISPLAY -depth $VNC_COL_DEPTH -geometry $VNC_RESOLUTION &> $STARTUPDIR/no_vnc_startup.log
-echo -e "start window manager\n..."
-$HOME/wm_startup.sh &> $STARTUPDIR/wm_startup.log
-
-## log connect options
-echo -e "\n\n------------------ VNC environment started ------------------"
-echo -e "\nVNCSERVER started on DISPLAY= $DISPLAY \n\t=> connect via VNC viewer with $VNC_IP:$VNC_PORT"
-echo -e "\nnoVNC HTML client started:\n\t=> connect via http://$VNC_IP:$NO_VNC_PORT/?password=...\n"
-
-
-if [[ $DEBUG == true ]] || [[ $1 =~ -t|--tail-log ]]; then
-    echo -e "\n------------------ $HOME/.vnc/*$DISPLAY.log ------------------"
-    # if option `-t` or `--tail-log` block the execution and tail the VNC log
-    tail -f $STARTUPDIR/*.log $HOME/.vnc/*$DISPLAY.log
-fi
-
-[[ $1 =~ -w|--wait ]] && shift
-
-if [ "$#" != "0" ]; then
-    $@ &     
-fi
-
-wait $PID_SUB
-
-exit 0
-
-if [ -z "$1" ] || [[ $1 =~ -w|--wait ]]; then
-    wait $PID_SUB
-else
-    # unknown option ==> call command
-    echo -e "\n\n------------------ EXECUTE COMMAND ------------------"
-    echo "Executing command: '$@'"
-    exec "$@"
-fi
